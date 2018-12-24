166e89ba3700b11cf4cab53825295548263ad0ae
enthusiastic-mouse-1767 <ebisiemen@gmail.com>
2018-12-24 10:45:04 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,31 +2,33 @@
 
 katz_deli = []
 
-def line (x)
+def line (katz_deli)
   line_array = []
-  if x.length == 0 
+  if katz_deli.length == 0 
     puts "The line is currently empty."
   else 
-    x.each.with_index do |name, index|
+    katz_deli.each.with_index do |name, index|
       line_array.push ( "#{index + 1}. #{name}" )
     end
     puts "The line is currently: #{line_array.join(" ") }"
 end 
 
-# task number 2 
+# task number 2 ticket counter starts from one 
 
-def take_a_number (katz_deli, name)
-  katz_deli.push (name)
-  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+number = 0 
+
+def take_a_number (katz_deli)
+  katz_deli.push (number = number + 1)  #shorthand number += 1 
+  puts "Welcome, you are number #{katz_deli.length} in line."
 end
 
 #task number 3
 
-def now_serving(array)
-  if array.empty?
+def now_serving(katz_deli)
+  if katz_deli.empty?
     puts "There is nobody waiting to be served!"
   else
-    puts  "Currently serving #{array.shift}."
+    puts  "Currently serving #{katz_deli.shift}."
     end
   end
  end
