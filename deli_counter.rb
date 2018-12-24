85ed39ee96827602fe8e7b6ad35accbc52df19ba
enthusiastic-mouse-1767 <ebisiemen@gmail.com>
2018-12-24 10:27:02 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,8 +7,8 @@ def line (x)
   if x.length == 0 
     puts "The line is currently empty."
   else 
-    x.each.with_index(1) do |name, index|
-      line_array.push ( "#{index}. #{name}" )
+    x.each.with_index do |name, index|
+      line_array.push ( "#{index + 1}. #{name}" )
     end
     puts "The line is currently: #{line_array.join(" ") }"
 end 
@@ -26,8 +26,7 @@ def now_serving(array)
   if array.empty?
     puts "There is nobody waiting to be served!"
   else
-    puts  "Currently serving #{array[0]}."
-    array.shift
+    puts  "Currently serving #{array.shift}."
     end
   end
  end
